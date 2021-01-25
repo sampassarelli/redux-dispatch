ff2843767cd3ebf86e915b6d3a9e8c6f1382516a
sampassarelli <passarelli.sam@gmail.com>
2021-01-24 21:56:50 -0600

    lab completed

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,22 @@
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+
+function dispatch(action){
+  state = changeState(state, action)
+  return state
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render(){
+  document.body.textContent = state.count
+}
 
-changeState(state, action)
+// call the render function
+render()
